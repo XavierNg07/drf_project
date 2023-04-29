b1955a81f8445ec35e39a41b5f4ee473b9054eb9
Xavier Nguyen <xaviernguyen07@gmail.com>
2023-04-29 12:26:43 -0700

    added serializers and ensure the tests pass

---
+++ movies/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+from .models import Movie
+
+
+# create a new class called MovieSerializer from a ModelSerializer
+# which outputs all the fields from the model
+class MovieSerializer(serializers.ModelSerializer):
+    # By identifying certain fields as "read only",
+    # we can ensure that they will never be created or updated via the serializer
+    class Meta:
+        model = Movie
+        fields = '__all__'
+        read_only_fields = ('id', 'created_date', 'updated_date',)
