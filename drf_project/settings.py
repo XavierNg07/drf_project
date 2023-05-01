9a26f417a9b34f8a754d211d6105e127776659eb
Xavier Nguyen <xaviernguyen07@gmail.com>
2023-05-01 01:17:10 -0700

    config Dockerfile.prod

--- drf_project/settings.py
+++ drf_project/settings.py
@@ -141,11 +141,11 @@ if not DEBUG:
 
     # Tell Django to copy statics to the `staticfiles` directory
     # in your application directory
-    # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-    #
-    # # Turn on WhiteNoise storage backend that takes care of compressing static files
-    # # and creating unique names for each version, so they can safely be cached forever
-    # STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+    # Turn on WhiteNoise storage backend that takes care of compressing static files
+    # and creating unique names for each version, so they can safely be cached forever
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
