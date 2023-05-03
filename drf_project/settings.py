7380558083d4a63249cd4c2e13469cde9dfa062f
Xavier Nguyen <xaviernguyen07@gmail.com>
2023-05-03 15:34:20 -0700

    added coverage.py

--- drf_project/settings.py
+++ drf_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = int(os.environ.get('DEBUG'))
+DEBUG = os.environ.get('DEBUG')
 
 # 'DJANGO_ALLOWED_HOSTS' should be a single string of hosts with a space between each.
 # For example: 'DJANGO_ALLOWED_HOSTS=localhost 127.0.0.1 [::1]'
