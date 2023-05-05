c34f5cde1b80b72be462afb554ac95a6821cfa80
Xavier Nguyen <xaviernguyen07@gmail.com>
2023-05-04 17:46:06 -0700

    modify for prod

--- drf_project/settings.py
+++ drf_project/settings.py
@@ -23,7 +23,7 @@ SECRET_KEY = os.environ.get(
 )
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG", "") != False
+DEBUG = os.environ.get("DEBUG", "") != "False"
 
 # 'DJANGO_ALLOWED_HOSTS' should be a single string of hosts with a space between each.
 # For example: 'DJANGO_ALLOWED_HOSTS=localhost 127.0.0.1 [::1]'
