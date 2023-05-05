5415c7a1f6e031577000373d70faa8c698ce6c0c
Xavier Nguyen <xaviernguyen07@gmail.com>
2023-05-04 18:06:55 -0700

    added railway url to allowed_hosts

--- drf_project/settings.py
+++ drf_project/settings.py
@@ -27,7 +27,7 @@ DEBUG = os.environ.get("DEBUG", "") != "False"
 
 # 'DJANGO_ALLOWED_HOSTS' should be a single string of hosts with a space between each.
 # For example: 'DJANGO_ALLOWED_HOSTS=localhost 127.0.0.1 [::1]'
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "[::1]"]
+ALLOWED_HOSTS = ["drfproject-production.up.railway.app", "localhost", "127.0.0.1", "[::1]"]
 
 # Application definition
 
