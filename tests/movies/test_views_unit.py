21f1574244bd1f235e837d9f0f19af8614fd2b3a
Xavier Nguyen <xaviernguyen07@gmail.com>
2023-05-12 10:21:04 -0700

    monkeypatching for post endpoint

--- tests/movies/test_views_unit.py
+++ tests/movies/test_views_unit.py
@@ -5,19 +5,35 @@ from movies.views import MovieSerializer, MovieDetail, Movie
 
 
 def test_add_movie(client, monkeypatch):
-    pass
+    payload = {"title": "The Big Lebowski", "genre": "comedy", "year": "1998"}
+
+    def mock_create():
+        return "The Big Lebowski"
+
+    monkeypatch.setattr(MovieSerializer, "create", mock_create)
+    monkeypatch.setattr(MovieSerializer, "data", payload)
+
+    res = client.post("/api/movies/", payload, content_type="application/json")
+    assert res.status_code == 201
+    assert res.data["title"] == "The Big Lebowski"
 
 
 def test_add_movie_invalid_json(client):
-    pass
+    res = client.post("/api/movies/", {}, content_type="application/json")
+    assert res.status_code == 400
 
 
 def test_add_movie_invalid_json_keys(client):
-    pass
+    res = client.post(
+        "/api/movies/",
+        {"title": "The Big Lebowski", "genre": "comedy"},
+        content_type="application/json",
+    )
+    assert res.status_code == 400
 
 
 def test_get_single_movie(client, monkeypatch):
-    pass
+    payload = {"title": "The Big Lebowski", "genre": "comedy", "year": "1998"}
 
 
 def test_get_single_movie_incorrect_id(client):
